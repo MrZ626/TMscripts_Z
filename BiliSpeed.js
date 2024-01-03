29a1395e117e63392e809222d9d4a7474ccc198a
MrZ_26 <1046101471@qq.com>
2024-01-03 17:32:20 +0800

    变速支持小键盘的加号，同时试图修找不到视频对象的bug虽然大概是没修好

--- BiliSpeed.js
+++ BiliSpeed.js
@@ -10,16 +10,25 @@
 // ==/UserScript==
 
 var video = document.getElementsByTagName("video")[0];
+var errCount = 0;
 
 function keyd(e) {
-    if (e.key == "=" || e.key == "-") {
-        var speed = video.playbackRate;
-        if (e.key == "=") speed = speed < 1 ? speed + 0.1 : speed + 0.25;
-        else speed = speed <= 1 ? speed - 0.1 : speed - 0.25;
-        video.playbackRate = speed.toFixed(2);
+    if (e.key == "=" || e.key == "-" || e.key == "+") {
+        if (video == null) video = document.getElementsByTagName("video")[0];
+        if (video) {
+            var speed = video.playbackRate;
+            if (e.key == "-") speed = speed <= 1 ? speed - 0.1 : speed - 0.25;
+            else speed = speed < 1 ? speed + 0.1 : speed + 0.25;
+            video.playbackRate = speed.toFixed(2);
 
-        var speedLabel = document.getElementsByClassName('bilibili-player-video-btn-speed-name')[0];
-        speedLabel.innerHTML = speed + "x";
+            var speedLabel = document.getElementsByClassName('bilibili-player-video-btn-speed-name')[0];
+            speedLabel.innerHTML = speed + "x";
+        } else {
+            if (errCount < 6) {
+                console.error("BiliSpeed: video not found");
+                errCount++;
+            }
+        }
     }
 }
 
